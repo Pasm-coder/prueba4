90253c1a2c67b06617795caf5ef9410a005d1e70
Pasm-coder <paolasarabia04@gmail.com>
2023-01-12 09:30:41 -0500

    second commit

--- prueba4/src/app/app-routing.module.ts
+++ prueba4/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WomenComponent } from './women/women.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'women',
+    component: WomenComponent,
+  },
+  { path: '', redirectTo: '/women', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
